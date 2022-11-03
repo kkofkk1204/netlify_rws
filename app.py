6452cdd9cf029c5de0ed4b789f702c952192821d
kkofkk1204 <tsuiwaichung903@gmail.com>
2022-11-03 14:55:02 +0800

    v3

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 from flask_restful import Api
 from sqlalchemy import create_engine
 from sqlalchemy import text
 import json
+import pymysql
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,10 +13,7 @@ CORS(app)
 
 @app.route('/')
 def hello_world():
-    # return 'Hello from Flask!'
 
-# @app.route("/test")
-# def testing_function():
     # DATABASE_URL  = 'mysql+mysqldb://admin:12345678@flask-backend.ckxv07dhtiyg.ap-northeast-1.rds.amazonaws.com:3306/'
     # engine = create_engine(DATABASE_URL, pool_recycle=280)
     # #with engine.connect() as connection:
@@ -26,7 +24,17 @@ def hello_world():
     # for post in blog_posts:
     #     x.append(post)
     # print(x)
-    return str('abc')
+    db_connect = pymysql.connect(
+    host='chungtsui.mysql.pythonanywhere-services.com',
+    user='chungtsui',
+    passwd='Kk27062779',
+    db='chungtsui$default',)
+
+    address = request.args.get('address')
+    cur = db_connect.cursor()
+    cur.execute(f'select * from ecdsa where address = "{address}";')
+    data = cur.fetchall()
+    return str(data)
 
 
 if __name__ == '__main__':
